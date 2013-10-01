dc01c12c538141df95f54b2090ec3de308aeaf48
carlio <git@carlcrowder.com>
2013-10-01 09:52:13 +0200

    Ignoring the constant name warning for "urlpatterns" in the test

--- test/input/func_noerror_urls.py
+++ test/input/func_noerror_urls.py
@@ -3,6 +3,9 @@ Checks that Pylint does not complain about attributes and methods
 when creating a typical urls.py
 """
 #  pylint: disable=C0111,R0903,W0232
+#  pylint: disable=C0103
+#    ^ eventually we should be able to override or update the
+#      CONST_NAME_RGX value
 __revision__ = ''
 
 from django.views.generic import TemplateView
