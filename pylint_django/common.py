edaf94277f38eb11fe38b06da51108f38548e983
carlio <git@carlcrowder.com>
2013-10-03 00:54:12 +0200

    Adding the dummy Model object to remove warnings about Model attributes which are dynamically created

--- pylint_django/common.py
+++ pylint_django/common.py
@@ -31,7 +31,6 @@ MANAGER.register_transform(nodes.Module, transform)
 
 
 def class_view_transform(module):
-
     # all django Views have the value of 'request', 'args' and 'kwargs' set in View.as_view()
 
     fake = AstroidBuilder(MANAGER).string_build('''
@@ -47,4 +46,17 @@ class View(object):
     module.locals['View'] = fake.locals['View']
 
 
+def model_transform(module):
+    fake = AstroidBuilder(MANAGER).string_build('''
+class Model:
+    objects = None
+    id = None
+    DoesNotExist = None
+    pk = None
+    _meta = None
+
+''')
+    module.locals['Model'] = fake.locals['Model']
+
 MODULE_TRANSFORMS['django.views.generic.base'] = class_view_transform
+MODULE_TRANSFORMS['django.db.models'] = model_transform
