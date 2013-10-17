c5341ce6e5d349f5372d245845f19ffa1ac4af81
carlio <git@carlcrowder.com>
2013-10-17 10:30:31 +0200

    Adding full dependency list on pylint and astroid

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description=_short_description,
     version=_version,
     packages=_packages,
-    install_requires=['pylint-plugin-utils'],
+    install_requires=['pylint', 'astroid', 'pylint-plugin-utils'],
     license='GPLv2',
     keywords=('pylint', 'django', 'plugin')
 )
