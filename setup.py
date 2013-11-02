7b14c16c37312c8b1a4247866e0bc7793d114738
carlio <git@carlcrowder.com>
2013-11-02 11:14:21 +0100

    Adding required voodoo invocations to include the transforms

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
+import os
 import time
 
 
@@ -11,6 +12,14 @@ _short_description = "pylint-django is a Pylint plugin to aid Pylint in recognis
                      "errors caused when using the Django framework"
 
 
+_transform_dir = 'pylint_django/transforms/transforms'
+_package_data = {
+    'pylint_django': [
+        os.path.join(_transform_dir, name) for name in os.listdir(_transform_dir)
+    ]
+}
+
+
 setup(
     name='pylint-django',
     url='https://github.com/landscapeio/pylint-django',
@@ -19,6 +28,7 @@ setup(
     description=_short_description,
     version=_version,
     packages=_packages,
+    package_data=_package_data,
     install_requires=['pylint>=1.0', 'astroid>=1.0', 'pylint-plugin-utils>=0.1'],
     license='GPLv2',
     keywords='pylint django plugin'
