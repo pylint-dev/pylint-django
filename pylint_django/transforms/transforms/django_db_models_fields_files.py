4f95d7b15ccd7bf0716783ebbff994fca1d451c7
carlio <git@carlcrowder.com>
2014-09-14 11:30:55 +0200

    [refs #12] Improving the type handling of FileField and ImageField files using a similar technique to #7 and the solution suggested by @enoren in #12 - namely, subclass both the Django Field type and the type that it is converted to by Django meta-programming

--- pylint_django/transforms/transforms/django_db_models_fields_files.py
+++ pylint_django/transforms/transforms/django_db_models_fields_files.py
@@ -1,9 +1,9 @@
-
-class FileField(object):
-    url = None
-    name = None
+from django.db.models.fields import files as django_fields
 
 
-class ImageField(object):
-    url = None
-    name = None
+class FileField(django_fields.FieldFile, django_fields.FileField):
+    pass
+
+
+class ImageField(django_fields.ImageFieldFile, django_fields.ImageField):
+    pass
