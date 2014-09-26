42c9bf33c00655b1eae3eaf6d79a458db9c81ab7
carlio <git@carlcrowder.com>
2014-09-26 08:44:03 +0200

    Adding some hacky shonk to speed up travis builds when nothing needs to actually run (due an incompatible combination of python version and django)

---
+++ scripts/travis-skip.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import os, sys
+for skip_ver in os.environ.get('SKIP', '').split():
+    skip_ver = tuple(map(int, skip_ver.split('.')))
+    if skip_ver == sys.version_info[:len(skip_ver)]:
+        sys.exit(1)
+sys.exit(0)
