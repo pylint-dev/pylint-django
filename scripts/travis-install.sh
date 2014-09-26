c4d255c90e4b11e0dc1c89eeec61dbbee38e9946
carlio <git@carlcrowder.com>
2014-09-26 08:44:03 +0200

    Adding some hacky shonk to speed up travis builds when nothing needs to actually run (due an incompatible combination of python version and django)

---
+++ scripts/travis-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python scripts/travis-skip.py
+
+if [ "$?" -eq "0" ]
+then
+    pip install --use-mirrors coverage coveralls
+    pip install --use-mirrors $DJANGO
+    pip install --use-mirrors git+https://github.com/landscapeio/pylint-plugin-utils.git@develop
+    pip install --use-mirrors --editable .
+else
+    echo "Skipping"
+fi
