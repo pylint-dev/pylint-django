886409788e751f4cfa426853b78e6ede853a3232
carlio <git@carlcrowder.com>
2014-10-06 07:19:32 +0200

    [refs #30] Adding a new checker and message to warn if Django is not on the PYTHONPATH. If django cannot be imported, then most of the inference fails, meaning pylint-django is basically useless

--- pylint_django/checkers/__init__.py
+++ pylint_django/checkers/__init__.py
@@ -1,7 +1,9 @@
 """Checkers."""
+from pylint_django.checkers.django_installed import DjangoInstalledChecker
 from pylint_django.checkers.models import ModelChecker
 
 
 def register_checkers(linter):
     """Register checkers."""
     linter.register_checker(ModelChecker(linter))
+    linter.register_checker(DjangoInstalledChecker(linter))
---
+++ pylint_django/checkers/django_installed.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import check_messages
+from pylint_django.__pkginfo__ import BASE_ID
+
+
+class DjangoInstalledChecker(BaseChecker):
+    name = 'django-installed-checker'
+
+    msgs = {
+        'F%s01' % BASE_ID: ("Django is not available on the PYTHONPATH",
+                            'django-not-available',
+                            "Django could not be imported by the pylint-django plugin, so most Django related "
+                            "improvements to pylint will fail."),
+
+        'W%s99' % BASE_ID: ('Placeholder message to prevent disabling of checker',
+                             'django-not-available-placeholder',
+                             'PyLint does not recognise checkers as being enabled unless they have at least'
+                             ' one message which is not fatal...')
+    }
+
+    @check_messages('django-not-available')
+    def close(self):
+        try:
+            import django as _  #  pylint: disable=F0401
+        except ImportError:
+            self.add_message('F%s01' % BASE_ID)
