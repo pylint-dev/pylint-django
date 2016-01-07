774d91ba4ec15939f972fba4efd358d0d628bbb6
Nils Berg <berg@magazino.eu>
2016-01-07 14:45:10 +0100

    fall back to astroid.nodes.Module.locals if _locals is not available

--- pylint_django/transforms/__init__.py
+++ pylint_django/transforms/__init__.py
@@ -26,7 +26,11 @@ def _add_transform(package_name, *class_names):
         if module.name != package_name:
             return
         for class_name in class_names:
-            module._locals[class_name] = fake._locals[class_name]  # pylint: disable=protected-access
+            # This changed from locals to _locals between astroid 1.3 and 1.4
+            if hasattr(module, '_locals'):
+                module._locals[class_name] = fake._locals[class_name]  # pylint: disable=protected-access
+            else:
+                module.locals[class_name] = fake.locals[class_name]
 
     MANAGER.register_transform(nodes.Module, set_fake_locals)
 
