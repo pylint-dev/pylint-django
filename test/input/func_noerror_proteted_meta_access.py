93fd04120d0690189c35b7b2eaace23117f388c5
carlio <git@carlcrowder.com>
2016-05-22 12:46:25 +0200

    [refs #66] Testing to see if access to _meta on models is allowed

---
+++ test/input/func_noerror_proteted_meta_access.py
@@ -0,0 +1,22 @@
+"""
+Tests to make sure that access to _meta on a model does not raise
+a protected-access warning, as it is part of the public API since
+Django 1.8
+(see https://github.com/landscapeio/pylint-django/issues/66,
+and https://docs.djangoproject.com/en/1.9/ref/models/meta/)
+"""
+#  pylint: disable=C0111,W5101,W5103
+from django.db import models
+
+
+class ModelWhichLikesMeta(models.Model):
+    ursuary = models.BooleanField(default=False)
+
+    def do_a_thing(self):
+        return self._meta.get_field('ursuary')
+
+
+if __name__ == '__main__':
+    MODEL = ModelWhichLikesMeta()
+    MODEL.save()
+    print MODEL.do_a_thing()
