2d71f0767373dda0c58e81c582884d860da9d9bd
carlio <git@carlcrowder.com>
2016-05-25 13:44:03 +0200

    Testing dropping of python 2.6 support, since pylint did that long ago and it is painful to try to keep compat with old versions of both

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ _classifiers = (
     'Intended Audience :: Developers',
     'Operating System :: Unix',
     'Topic :: Software Development :: Quality Assurance',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
