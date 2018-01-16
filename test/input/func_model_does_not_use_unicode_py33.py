a8f668bcb9b3b5696c7f8f97f7d973da1eb9fc53
Mr. Senko <atodorov@mrsenko.com>
2018-01-16 22:38:41 +0200

    Update format of expected error messages for unicode test
    the latest version of pylint expects the error messages in
    slightly different format.

--- test/input/func_model_does_not_use_unicode_py33.py
+++ test/input/func_model_does_not_use_unicode_py33.py
@@ -6,7 +6,7 @@ Ensures that under PY3 django models with a __unicode__ method are flagged
 from django.db import models
 
 
-class SomeModel(models.Model):
+class SomeModel(models.Model): # [model-has-unicode]
     something = models.CharField(max_length=255)
     # no __str__ method
 
