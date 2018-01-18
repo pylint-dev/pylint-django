f252f0ab48cd5b19c61d397006b07fb50d17d254
Mr. Senko <atodorov@mrsenko.com>
2018-01-18 16:43:22 +0200

    Issue DeprecationWarning when running on Python 2

--- pylint_django/__init__.py
+++ pylint_django/__init__.py
@@ -1,5 +1,13 @@
 """pylint_django module."""
 from __future__ import absolute_import
+
+import sys
+import warnings
+
 from pylint_django import plugin
+
+if sys.version_info < (3, ):
+    warnings.warn("Version 0.8.0 is the last to support Python 2. "
+                  "Please migrate to Python 3!", DeprecationWarning)
 
 register = plugin.register
