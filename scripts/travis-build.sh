fa083d0b8f2a832e2886e6812751bc94415b4e1f
Mr. Senko <atodorov@mrsenko.com>
2018-01-18 11:53:55 +0200

    Update test matrix and how tests are started on Travis

--- scripts/travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python scripts/travis_skip.py
-
-if [ "$?" -eq "0" ]
-then
-    coverage run test/test_func.py
-else
-    echo "Skipping"
-fi
--- scripts/travis_skip.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-for skip_ver in os.environ.get('SKIP', '').split():
-    skip_ver = tuple(map(int, skip_ver.split('.')))
-    if skip_ver == sys.version_info[:len(skip_ver)]:
-        sys.exit(1)
-sys.exit(0)
