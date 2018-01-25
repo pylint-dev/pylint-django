84a007076f4906523b2e6b93d827cb4836f209c4
Mr. Senko <atodorov@mrsenko.com>
2018-01-25 14:16:46 +0200

    Add test for #117

---
+++ pylint_django/tests/input/external_model_utils_noerror_override_manager.py
@@ -0,0 +1,26 @@
+# Check that when overriding the 'objects' attribite of a model class
+# with a manager from the django-model-utils package pylint-django
+# does not report not-an-iterator error
+# pylint: disable=missing-docstring
+
+from model_utils.managers import InheritanceManager
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    name = models.CharField()
+
+
+class BuggyModel(models.Model):
+    objects = InheritanceManager()
+
+    name = models.CharField()
+
+
+def function():
+    for record in BaseModel.objects.all():
+        print(record.name)
+
+    for record in BuggyModel.objects.all():
+        print(record.name)
