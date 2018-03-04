98e5091a04859ea65bfa06f3166af3b2951f5fb5
Mr. Senko <atodorov@mrsenko.com>
2018-03-04 23:26:37 +0200

    Update MANIFEST.in and setup.py
    - setup.py: move all the values directly as parameters to setup()
      to make it more readable and avoid scrolling back and forth to
      figure out what comes from where
    - MANIFEST.in: add Markdown files to the list of included files,
      e.g. CHANGELOG, CONTRIBUTORS, README; adjust how we include the
      supplementary files for tests

--- setup.py
+++ setup.py
@@ -5,52 +5,30 @@ Setup module for Pylint plugin for Django.
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.9.1'
-PACKAGES = find_packages(exclude=[
-    '*.tests',
-    '*.tests.*',
-    'tests.*',
-    'tests',
-])
-
-SHORT_DESCRIPTION = 'A Pylint plugin to help Pylint understand the Django web framework'
-
-TRANSFORM_DIR = 'pylint_django/transforms/transforms'
-PACKAGE_DATA = {
-    'pylint_django': [
-        os.path.join('transforms/transforms', name) for name in os.listdir(TRANSFORM_DIR)
-    ]
-}
-
-CLASSIFIERS = (
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'Operating System :: Unix',
-    'Topic :: Software Development :: Quality Assurance',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-)
-
-INSTALL_REQUIRES = [
-    'pylint-plugin-utils>=0.2.1',
-    'pylint>=1.8.2',
-]
-
 
 setup(
     name='pylint-django',
     url='https://github.com/PyCQA/pylint-django',
     author='landscape.io',
     author_email='code@landscape.io',
-    description=SHORT_DESCRIPTION,
-    version=VERSION,
-    packages=PACKAGES,
-    package_data=PACKAGE_DATA,
-    install_requires=INSTALL_REQUIRES,
+    description='A Pylint plugin to help Pylint understand the Django web framework',
+    version='0.9.2',
+    packages=find_packages(),
+    install_requires=[
+        'pylint-plugin-utils>=0.2.1',
+        'pylint>=1.8.2',
+    ],
     license='GPLv2',
-    classifiers=CLASSIFIERS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Quality Assurance',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     keywords=['pylint', 'django', 'plugin'],
     zip_safe=False,
 )
