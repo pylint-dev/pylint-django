d1d6f404c3184ae6d392048abc71cbc6d6a36816
Mr. Senko <atodorov@mrsenko.com>
2018-04-18 00:26:26 +0300

    New JsonResponse checker
    to validate common anti-patterns with http responses returning
    JSON data

--- pylint_django/checkers/__init__.py
+++ pylint_django/checkers/__init__.py
@@ -1,9 +1,11 @@
 """Checkers."""
 from pylint_django.checkers.django_installed import DjangoInstalledChecker
 from pylint_django.checkers.models import ModelChecker
+from pylint_django.checkers.json_response import JsonResponseChecker
 
 
 def register_checkers(linter):
     """Register checkers."""
     linter.register_checker(ModelChecker(linter))
     linter.register_checker(DjangoInstalledChecker(linter))
+    linter.register_checker(JsonResponseChecker(linter))
