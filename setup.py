d49c01fe178fb58a5887d372e54f26dc39e7fb41
Mr. Senko <atodorov@mrsenko.com>
2018-05-25 00:38:05 +0300

    Changelog for version 0.11.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email='code@landscape.io',
     description='A Pylint plugin to help Pylint understand the Django web framework',
     long_description=LONG_DESCRIPTION,
-    version='0.11',
+    version='0.11.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
