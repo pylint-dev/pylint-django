4360d793d95db615c6f7d4e69e02d280a7bb1abd
Mr. Senko <atodorov@mrsenko.com>
2018-05-24 23:44:31 +0300

    Remove disable=invalid-name for urlpatterns test. Refs #161
    The `urlpatterns` constant has been supported since version 0.8.0.
    This commit makes sure the checker isn't disabled for this test
    file.

--- pylint_django/tests/input/func_noerror_urls.py
+++ pylint_django/tests/input/func_noerror_urls.py
@@ -3,9 +3,6 @@ Checks that Pylint does not complain about attributes and methods
 when creating a typical urls.py
 """
 #  pylint: disable=missing-docstring
-#  pylint: disable=C0103
-#    ^ eventually we should be able to override or update the
-#      CONST_NAME_RGX value
 
 from django.views.generic import TemplateView
 from django.conf.urls import url
