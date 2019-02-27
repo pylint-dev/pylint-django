74bd99bf147e6b6637d576a5c3523e3df62ede4a
Carl Crowder <carl@redcliff.ltd>
2019-02-27 22:14:25 +0100

    Including coverage in test dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     extras_require={
         'with_django': ['Django'],
-        'for_tests': ['django_tables2', 'factory-boy'],
+        'for_tests': ['django_tables2', 'factory-boy', 'coverage'],
     },
     license='GPLv2',
     classifiers=[
