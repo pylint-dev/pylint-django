27b8c81bdb13061728b04e17e59a364df01f541e
Mr. Senko <atodorov@mrsenko.com>
2019-04-26 12:49:26 +0300

    Add new test for GenericForeignKey. Refs #230, #224

---
+++ pylint_django/tests/input/func_noerror_generic_foreign_key.py
@@ -0,0 +1,17 @@
+"""
+Checks that Pylint does not complain about GenericForeignKey fields:
+https://github.com/PyCQA/pylint-django/issues/230
+"""
+# pylint: disable=missing-docstring
+
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Ownership(models.Model):
+    # for #230 the important bit is this FK field which doesn't
+    # have any keyword arguments!
+    owner_type = models.ForeignKey(ContentType, models.CASCADE)
+    owner_id = models.PositiveIntegerField()
+    owner = GenericForeignKey("owner_type", "owner_id")
