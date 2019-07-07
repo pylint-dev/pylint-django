c3fabb4e452395f2873d016047ba0f59d173ad6b
Mr. Senko <atodorov@mrsenko.com>
2019-07-07 14:37:10 +0300

    Disable no-member in test file
    b/c this test file is only to validate we don't get the traceback
    seen in https://github.com/PyCQA/pylint-django/issues/232

--- pylint_django/tests/input/models/func_noerror_foreign_key_key_cls_unbound_in_same_package.py
+++ pylint_django/tests/input/models/func_noerror_foreign_key_key_cls_unbound_in_same_package.py
@@ -1,8 +1,16 @@
 """
-Checks that Pylint does not complain about ForeignKey pointing to model
-in module of models package
+Checks that Pylint does not crash with ForeignKey string reference pointing to model
+in module of models package. See
+https://github.com/PyCQA/pylint-django/issues/232
+
+Note: the no-member disable is here b/c pylint-django doesn't know how to
+load models.author.Author. When pylint-django tries to load models referenced
+by a single string it assumes they are found in the same module it is inspecting.
+Hence it can't find the Author class here so it tells us it doesn't have an
+'id' attribute. Also see:
+https://github.com/PyCQA/pylint-django/issues/232#issuecomment-495242695
 """
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring, no-member
 from django.db import models
 
 
@@ -12,4 +20,4 @@ class FairyTail(models.Model):
     author = models.ForeignKey(to='Author', null=True, on_delete=models.CASCADE)
 
     def get_author_name(self):
-        return self.author.id
+        return self.author.id  # disable via no-member
