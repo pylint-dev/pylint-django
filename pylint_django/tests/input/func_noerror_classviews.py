4014bae95f9f6e9445788ccbbd4a96f75b88c7ed
Mr. Senko <atodorov@mrsenko.com>
2019-11-23 12:22:23 +0200

    tests: Add class-based generic views
    DetailView and CreateView descendants

--- pylint_django/tests/input/func_noerror_classviews.py
+++ pylint_django/tests/input/func_noerror_classviews.py
@@ -4,8 +4,12 @@ when using Class-based Views
 """
 #  pylint: disable=missing-docstring
 
+from django.db import models
 from django.http import JsonResponse
-from django.views.generic import TemplateView, View
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
+from django.views.generic import View
+from django.views.generic.edit import CreateView
 
 
 class BoringView(TemplateView):
@@ -23,3 +27,24 @@ class JsonView(View):
         # do something with objects but don't use
         # self or request
         return JsonResponse({'rc': 0, 'response': 'ok'})
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    good = models.BooleanField(default=False)
+
+
+class GetBook(DetailView):
+    model = Book
+    template_name = 'books/get.html'
+    http_method_names = ['get']
+
+
+class CreateBook(CreateView):
+    model = Book
+    template_name = 'books/new.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'New book'
+        return context
