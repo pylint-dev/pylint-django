8454da611a8b02797c7784987a9ac4395139e104
Alexander Todorov <atodorov@mrsenko.com>
2021-04-26 12:46:55 +0300

    Fix for pylint >= 2.8. Fixes #322, #323
    not only variable names have changed, but also tuple values are now
    string instead of integers.

--- pylint_django/compat.py
+++ pylint_django/compat.py
@@ -25,4 +25,8 @@ except ImportError:
 import pylint
 
 # pylint before version 2.3 does not support load_configuration() hook.
-LOAD_CONFIGURATION_SUPPORTED = pylint.__pkginfo__.numversion >= (2, 3)
+LOAD_CONFIGURATION_SUPPORTED = False
+try:
+    LOAD_CONFIGURATION_SUPPORTED = tuple(pylint.__version__.split('.')) >= ('2', '3')
+except AttributeError:
+    LOAD_CONFIGURATION_SUPPORTED = pylint.__pkginfo__.numversion >= (2, 3)
