342c591eda1233640e8b6ef4b294d24bc78f91fe
Carl Crowder <carl@zerfeciz.com>
2023-10-22 08:39:33 +0700

    tmp will be squashed, CI debug by YAML hell

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# TODO : find out why tox refuses to install this or install any dev dependencies from poetry :-|
+pip install 'pytest>=7.3.1'
+
 python pylint_django/tests/test_func.py -v "$@"
